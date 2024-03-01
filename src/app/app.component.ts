9ab5f08492e857fa2e9afc713b42ab0017d3e8c8
Dewmith Akalanka <akalankadewmith@gmail.com>
2024-03-01 23:14:10 +0530

    add basic form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,38 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup } from '@angular/forms';
+import axios from 'axios';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'form-app';
+  selectedFile;
+
+  userDataForm = new FormGroup({
+    name: new FormControl(''),
+    email: new FormControl(''),
+    paymentImage: new FormControl(null),
+  });
+
+  async onSubmit() {
+    const formData = new FormData();
+    formData.append('name', this.userDataForm.value.name);
+    formData.append('email', this.userDataForm.value.email);
+    formData.append('paymentImage', this.selectedFile);
+    console.log('Form data:', formData.get('paymentImage'));
+
+    const response = await axios.post(
+      'https://form-app-server-uh4d.onrender.com/api/submit',
+      formData
+    );
+    console.log(response);
+  }
+
+  onFileChange(event: any) {
+    this.selectedFile = event.target.files[0];
+    this.userDataForm.get('paymentImage').setValue(this.selectedFile);
+  }
 }
